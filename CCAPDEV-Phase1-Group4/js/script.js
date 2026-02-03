78d5a502de87c939d226290f51fda9ec83551100
gabe-0017 <john_gabriel_calderon@dlsu.edu.ph>
2026-02-03 14:40:37 +0800

    Add files via upload

---
+++ CCAPDEV-Phase1-Group4/js/script.js
@@ -0,0 +1,19 @@
+/* <script defer src="js/script.js"></script> */
+document.addEventListener("DOMContentLoaded", function() {
+
+    console.log("Lab Reservation System Loaded");
+
+});
+
+/* placeholder login */
+function loginUser() {
+
+    alert("Login successful! (Demo only)");
+    window.location.href = "dashboard.html";
+}
+
+/* placeholder reservation */
+function submitReservation() {
+
+    alert("Reservation submitted successfully!");
+}
